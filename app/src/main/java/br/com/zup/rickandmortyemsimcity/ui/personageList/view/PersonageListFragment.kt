c643ae0aaaf3c9fb67ea9e1ce13bdb7077d2d512
TCriGa <thay.c.gayv@gmail.com>
2022-07-08 15:20:26 -0300

    Atualizar Desafio Rick and Morty - Criar recycleView, personage_list_item.xml, PersonageListFragment.kt

--- app/src/main/java/br/com/zup/rickandmortyemsimcity/ui/personageList/view/PersonageListFragment.kt
+++ app/src/main/java/br/com/zup/rickandmortyemsimcity/ui/personageList/view/PersonageListFragment.kt
@@ -1,12 +1,11 @@
 package br.com.zup.rickandmortyemsimcity.ui.personageList.view
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import br.com.zup.rickandmortyemsimcity.R
-
 
 class PersonageListFragment : Fragment() {
 
